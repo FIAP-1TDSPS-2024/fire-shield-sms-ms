da0126b290869cc36ab055ed77fa379f8f224ec3
jonasdasneves <jonas.joliveira2024@gmail.com>
2026-05-31 23:47:48 -0300

    (feat) Criação de projeto e definição de dependências iniciais

---
+++ src/test/java/br/com/catech/fire_shield_sms_ms/FireShieldSmsMsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.catech.fire_shield_sms_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FireShieldSmsMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
